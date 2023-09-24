21231c565cc3389ba6a72918f293585cc75d632b
diya17 <diyanna.3@gmail.com>
2023-09-23 21:27:59 -0700

    Update app.py to handle cookies for a user

--- web-tier/app.py
+++ web-tier/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from flask_bootstrap import Bootstrap
 from model.FilesForm import FilesForm
 from werkzeug.utils import secure_filename
+import json
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -10,17 +11,22 @@ allowedFileExtensions = ['JPEG']
 @app.route('/', methods=['GET', 'POST'])
 def index():
     uploadFilesForm = FilesForm(request.form)
+    resp = make_response(render_template("index.html", form=uploadFilesForm))
     if request.method == "POST" and uploadFilesForm.validate():
+        listOfFiles = []
+        if request.cookies.get('uploadedFiles') is not None:
+            listOfFiles = json.loads(request.cookies.get('uploadedFiles'))
         for uploadedFile in request.files.getlist('files'):
-            if uploadedFile.content_type != 'image/jpeg' or uploadedFile.content_type != 'image/png':
+            if uploadedFile.content_type != 'image/jpeg' and uploadedFile.content_type != 'image/png':
                 uploadFilesForm.files.errors.append(uploadedFile.filename + " Files Need to be a jpeg or png image.")
                 continue
 
             if len(uploadedFile.filename) > 0:
                 fileName = secure_filename(uploadedFile.filename)
+                listOfFiles.append(fileName)
+        resp.set_cookie('uploadedFiles', json.dumps(listOfFiles))
 
-
-    return render_template("index.html", form=uploadFilesForm)
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True)
