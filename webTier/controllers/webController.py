8dddd652af82267897fe26db2b521d9b8ed0d380
diya17 <diyanna.3@gmail.com>
2023-09-25 08:58:55 -0700

    Making web and api controllers

--- webTier/controllers/webController.py
+++ webTier/controllers/webController.py
@@ -1,16 +1,12 @@
 from flask import Flask, render_template, request, make_response
 from flask_bootstrap import Bootstrap
 import json
-from model.FilesForm import FilesForm
+from webTier.model.FilesForm import FilesForm
 import os
-from service import uploadService as uploadService
-from utils import s3 as s3Util
-from werkzeug.utils import secure_filename
+from webTier.service import uploadService as uploadService
+from webTier import app
 
-
-app = Flask(__name__)
 Bootstrap(app)
-INPUT_BUCKET_NAME = os.getenv("INPUT_BUCKET_NAME")
 @app.route('/', methods=['GET', 'POST'])
 def index():
     uploadFilesForm = FilesForm(request.form)
@@ -24,6 +20,3 @@ def index():
         resp.set_cookie('uploadedFiles', json.dumps(uploadedFilesForCookies))
 
     return resp
-
-if __name__ == '__main__':
-    app.run(debug=True)
