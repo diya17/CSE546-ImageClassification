61768f7e5ac7003af3170a7d862f2c51d08d06b5
diya17 <diyanna.3@gmail.com>
2023-10-05 20:21:31 -0700

    app scaling service

---
+++ webTier/service/appScalingService.py
@@ -0,0 +1,28 @@
+import utils.ec2 as ec2Util
+import utils.sqs as sqsUtil
+import time
+
+SQS_IMAGE_CLASSIFICATION_INPUT_QUEUE_URL = os.getenv("SQS_IMAGE_CLASSIFICATION_INPUT_QUEUE_URL")
+MESSAGE_THRESHOLD = os.getenv("MESSAGE_THRESHOLD")
+appTierInstanceId = 1
+prevQueueSize = 0
+def scaleServiceUp():
+    while(True):
+        currentQueueSize = sqsUtil.getNumberOfQueueMessages(SQS_IMAGE_CLASSIFICATION_INPUT_QUEUE_URL)
+        print("Current number of messages in the queue " + currentQueueSize)
+        numberOfPendingOrRunningInstances = ec2Util.getCountOfPendingOrRunningInstances()
+        print("Current number of instances in the app-tier " + numberOfPendingOrRunningInstances)
+
+        newMessagesDelta = currentQueueSize - prevQueueSize
+        prevQueueSize = currentQueueSize
+        if newMessagesDelta > MESSAGE_THRESHOLD:
+            numberOfInstancesToCreate = min(newMessagesDelta, 19-numberOfPendingOrRunningInstances)
+            createEC2Instances(numberOfInstancesToCreate)
+        time.sleep(5)
+
+def createEC2Instances(numberOfInstancesToCreate):
+    for i in range(1, numberOfInstancesToCreate+1):
+        ec2Util.createEC2Instance(appTierInstanceId)
+        appTierInstanceId = (appTierInstanceId+1)%19
+        if appTierInstanceId == 0:
+            appTierInstanceId = 1
