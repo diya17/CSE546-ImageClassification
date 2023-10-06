d0badfa1ec22352cbf2305b99b53bf0ffefeb4f9
diya17 <diyanna.3@gmail.com>
2023-10-05 20:49:01 -0700

    update scaling service

--- webTier/service/appScalingService.py
+++ webTier/service/appScalingService.py
@@ -3,29 +3,31 @@ import utils.sqs as sqsUtil
 import time
 import os
 
-SQS_IMAGE_CLASSIFICATION_INPUT_QUEUE_URL = os.getenv("SQS_IMAGE_CLASSIFICATION_INPUT_QUEUE_URL")
-MESSAGE_THRESHOLD = os.getenv("MESSAGE_THRESHOLD")
-appTierInstanceId = 1
-def scaleServiceUp():
-    prevQueueSize = 0
-    while(True):
-        currentQueueSize = sqsUtil.getNumberOfQueueMessages(SQS_IMAGE_CLASSIFICATION_INPUT_QUEUE_URL)
-        print("Current number of messages in the queue " + str(currentQueueSize))
-        numberOfPendingOrRunningInstances = ec2Util.getCountOfPendingOrRunningInstances()
-        if numberOfPendingOrRunningInstances is None:
-            numberOfPendingOrRunningInstances = 0
-        print("Current number of instances in the app-tier " + str(numberOfPendingOrRunningInstances))
+class AppScalingService():
+    SQS_IMAGE_CLASSIFICATION_INPUT_QUEUE_URL = os.getenv("SQS_IMAGE_CLASSIFICATION_INPUT_QUEUE_URL")
+    MESSAGE_THRESHOLD = os.getenv("MESSAGE_THRESHOLD")
+    def __init__(self):
+        self.prevQueueSize = 0
+        self.appTierInstanceId = 1
+    def scaleServiceUp(self):
+        while (True):
+            currentQueueSize = sqsUtil.getNumberOfQueueMessages(SQS_IMAGE_CLASSIFICATION_INPUT_QUEUE_URL)
+            print("Current number of messages in the queue " + str(currentQueueSize))
+            numberOfPendingOrRunningInstances = ec2Util.getCountOfPendingOrRunningInstances()
+            if numberOfPendingOrRunningInstances is None:
+                numberOfPendingOrRunningInstances = 0
+            print("Current number of instances in the app-tier " + str(numberOfPendingOrRunningInstances))
 
-        newMessagesDelta = currentQueueSize - prevQueueSize
-        prevQueueSize = currentQueueSize
-        if newMessagesDelta > int(MESSAGE_THRESHOLD):
-            numberOfInstancesToCreate = min(newMessagesDelta, 19-numberOfPendingOrRunningInstances)
-            createEC2Instances(numberOfInstancesToCreate)
-        time.sleep(5)
+            newMessagesDelta = currentQueueSize - self.prevQueueSize
+            self.prevQueueSize = currentQueueSize
+            if newMessagesDelta > int(MESSAGE_THRESHOLD):
+                numberOfInstancesToCreate = min(newMessagesDelta, 19 - numberOfPendingOrRunningInstances)
+                self.createEC2Instances(numberOfInstancesToCreate)
+            time.sleep(5)
 
-def createEC2Instances(numberOfInstancesToCreate):
-    for i in range(1, numberOfInstancesToCreate+1):
-        ec2Util.createEC2Instance(appTierInstanceId)
-        appTierInstanceId = (appTierInstanceId+1)%19
-        if appTierInstanceId == 0:
-            appTierInstanceId = 1
+    def createEC2Instances(self, numberOfInstancesToCreate):
+        for i in range(1, numberOfInstancesToCreate + 1):
+            ec2Util.createEC2Instance(self.appTierInstanceId)
+            self.appTierInstanceId = (appTierInstanceId + 1) % 19
+            if self.appTierInstanceId == 0:
+                self.appTierInstanceId = 1
