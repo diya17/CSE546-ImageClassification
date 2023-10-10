44a70cf4cc4fbe6aef5432c0ea540d3fda3301be
diya17 <diyanna.3@gmail.com>
2023-10-10 09:00:19 -0700

    update api

--- webTier/controllers/apiController.py
+++ webTier/controllers/apiController.py
@@ -10,12 +10,11 @@ usersToFileMap = collections.defaultdict(set)
 def uploadFiles():
     userIp = request.remote_addr
     if userIp:
-        if len(request.files.getlist('files')) > 1:
-            uploadService.processUploadFileForApi(request.files.getlist('files'), userIp, usersToFileMap, len(request.files.getlist('files')))
-            result = resultsService.generateResultsForUser(userIp, usersToFileMap)
-            return jsonify(result)
-        elif len(request.files.getlist('files')) == 1:
-            resultFileName = uploadService.processUploadFileForApi(request.files.getlist('files'), userIp, usersToFileMap, len(request.files.getlist('files')))
-            result = resultsService.generateResultForFile(resultFileName)
-            return result
+        uploadService.processUploadFileForApi(request.files.getlist('files'), userIp, usersToFileMap, len(request.files.getlist('files')))
+        result = resultsService.generateResultsForUser(userIp, usersToFileMap)
+        return jsonify(result)
+    elif len(request.files.getlist('files')) == 1:
+        resultFileName = uploadService.processUploadFileForApi(request.files.getlist('files'), userIp, usersToFileMap, len(request.files.getlist('files')))
+        result = resultsService.generateResultForFile(resultFileName)
+        return result
     return "Invalid File Upload"
